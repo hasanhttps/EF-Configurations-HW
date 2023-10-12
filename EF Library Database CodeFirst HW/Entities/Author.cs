169882c7bfc4c16ef7ac937fe0256a83f05763f7
hasanhttps <hasanabdullazad@gmail.com>
2023-10-12 23:42:58 +0400

    Library Database

---
+++ EF Library Database CodeFirst HW/Entities/Author.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Practise.Entities;
+
+public class Author : BaseEntity {
+    
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    // Navigation property
+
+    public ICollection<Book> Books { get; set; }
+
+}
---
+++ EF Library Database CodeFirst HW/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace EF_Library_Database_CodeFirst_HW {
+    internal class Program {
+        static void Main() {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
